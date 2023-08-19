49efc660d9fc1321a8967f916016399ece7dbbac
Taurus <jojirocyber@gmail.com>
2023-08-19 13:46:23 +0200

    initial

--- src/main/java/org/kardinal/Main.java
+++ src/main/java/org/kardinal/Main.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        String token = "MTE0MjkwNDY1NjI4NTE1MTM4NA.GuSrUi.tLRzIz006TtpsktAb46CotSLEOhCh6nmGvQWcU";
+        String token = "";
 
         JDABuilder b = JDABuilder.createDefault(token).setStatus(OnlineStatus.ONLINE).setActivity(Activity.playing("rarr")).addEventListeners(new Commands());
         b.setEnabledIntents(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS));
